d6d6374c779a62c5af19f69d1b0513b4d27bbfbe
Shubham Tomar <shubham.tomar@nagarro.com>
2021-12-08 12:10:34 +0530

    init

---
+++ toast-notification/script.js
@@ -0,0 +1,20 @@
+const btn=document.getElementById("btn");
+const conatiner=document.getElementById("container");
+
+btn.addEventListener("click",()=>{
+    createNotification();
+});
+
+
+function createNotification(){
+    const notif=document.createElement("div");
+    notif.classList.add("toast");
+
+    notif.innerText="This challenge is crazy!";
+
+    conatiner.appendChild(notif);
+
+    setTimeout(()=>{
+        notif.remove();
+    },3000);
+}
